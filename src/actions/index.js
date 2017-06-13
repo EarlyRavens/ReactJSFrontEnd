fbb7654967bb83d7c941e9eef6a87162fbff676b
Max Peiros <mpeiros@gmail.com>
2017-06-12 17:50:41 -0700

    Add heroku url to axios request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,25 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 export const FETCH_SEARCH_RESULTS = 'fetch_search_results';
 
-// const ROOT_URL = 'http://herokuapp.com/';
+const ROOT_URL = 'https://earlybirdsearch.herokuapp.com/?';
 
 export function fetchSearchResults(term, location) {
-  // const request = axios.get(`${ROOT_URL}/${term}/${location}`);
+  const request = axios.get(`${ROOT_URL}business=${term}&location=${location}`);
 
-  const testJSON = { 
-    data: [ 
-            { name: 'Michael Mina', url: 'yelp.com/michaelminna' },
-            { name: 'Sushi', url: 'yelp.com/sushi' },
-            { name: 'Thai', url: 'yelp.com/thai' },
-            { name: 'Chinese', url: 'yelp.com/chinese' } 
-          ] 
-  };
+  console.log(request);
+
+  // const testJSON = { 
+  //   data: [ 
+  //           { name: 'Michael Mina', url: 'yelp.com/michaelminna' },
+  //           { name: 'Sushi', url: 'yelp.com/sushi' },
+  //           { name: 'Thai', url: 'yelp.com/thai' },
+  //           { name: 'Chinese', url: 'yelp.com/chinese' } 
+  //         ] 
+  // };
 
   return {
     type: FETCH_SEARCH_RESULTS,
-    payload: testJSON
+    payload: request
   }
 }
