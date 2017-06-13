f774219d07275d464f717815975145bc71f65154
Max Peiros <mpeiros@gmail.com>
2017-06-13 12:15:28 -0700

    Remove commented out localhost code

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 export const FETCH_SEARCH_RESULTS = 'fetch_search_results';
 
 const ROOT_URL = 'https://earlybirdsearch.herokuapp.com/?';
-// const LOCAL_HOST = 'http://localhost:3000/?'
 
 export function fetchSearchResults(term, location) {
   const request = axios.get(`${ROOT_URL}business=${term}&location=${location}`);
-  // const request = axios.get(`${LOCAL_HOST}business=${term}&location=${location}`);
 
   return {
     type: FETCH_SEARCH_RESULTS,
