f40dccc5ea95a9fc7a0255869dc212d675107403
Max Peiros <mpeiros@gmail.com>
2017-06-14 17:58:37 -0700

    Add navbar with links to home page

--- src/containers/search_index.js
+++ src/containers/search_index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/search_bar';
 import ResultList from '../components/result_list';
+import NavBar from '../components/nav_bar';
 
 class SearchIndex extends Component {
   render() {
     return (
-      <div className="container col-md-6 col-md-offset-3 text-center">
-        <SearchBar />
-        <ResultList />
+      <div>
+        <NavBar />
+        <div className="container col-md-6 col-md-offset-3 text-center">
+          <SearchBar />
+          <ResultList />
+        </div>
       </div>
     );
   }
